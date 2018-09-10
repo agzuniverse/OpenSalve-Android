9bb1a323d910eb138c0d3d6b6e108369d67337ff
Aswin G <aswinganesh666@gmail.com>
2018-09-11 01:34:28 +0530

    Made a recycler view for displaying supplies needed

--- app/src/main/java/com/agzuniverse/agz/opensalve/CampMgmtScreen.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/CampMgmtScreen.java
@@ -2,6 +2,10 @@ package com.agzuniverse.agz.opensalve;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.agzuniverse.agz.opensalve.adapters.SuppliesNeededAdapter;
 
 public class CampMgmtScreen extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class CampMgmtScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camp_mgmt_home);
+
+        //TODO Set camp name, image, contact and camp manager's name
+        //TODO fetch list of supplies needed for camp and display it
+        //TODO Replace this dummy array with the fetched data
+        String[] data = {"Snacks", "Drinking Water", "Clothes", "Paracetamol", "First Aid kits"};
+
+        RecyclerView list = findViewById(R.id.camp_supplies);
+        list.setHasFixedSize(true);
+        RecyclerView.LayoutManager listManager = new LinearLayoutManager(this);
+        RecyclerView.Adapter listAdapter = new SuppliesNeededAdapter(this, data);
+        list.setAdapter(listAdapter);
+        list.setLayoutManager(listManager);
+
     }
 }
--- app/src/main/java/com/agzuniverse/agz/opensalve/adapters/SuppliesNeededAdapter.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/adapters/SuppliesNeededAdapter.java
@@ -1,4 +1,51 @@
 package com.agzuniverse.agz.opensalve.adapters;
 
-public class SuppliesNeededAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.agzuniverse.agz.opensalve.R;
+
+public class SuppliesNeededAdapter extends RecyclerView.Adapter<SuppliesNeededAdapter.SuppliesNeededViewHolder> {
+
+    private LayoutInflater inflater;
+    private String[] supplies;
+
+    public SuppliesNeededAdapter(Context context, String[] supplies) {
+        inflater = LayoutInflater.from(context);
+        this.supplies = supplies;
+    }
+
+    @NonNull
+    @Override
+    public SuppliesNeededViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = inflater.inflate(R.layout.supplies_needed_row, viewGroup, false);
+        SuppliesNeededViewHolder holder = new SuppliesNeededViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SuppliesNeededViewHolder viewHolder, int i) {
+        String current = supplies[i];
+        viewHolder.supply.setText(current);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class SuppliesNeededViewHolder extends RecyclerView.ViewHolder {
+        public TextView supply;
+
+        public SuppliesNeededViewHolder(@NonNull View itemView) {
+            super(itemView);
+            supply = itemView.findViewById(R.id.supply);
+        }
+    }
 }
