a20717708a0bc35b52de5aa0c537d14b41ec268a
Aswin G <aswinganesh666@gmail.com>
2018-09-21 00:43:38 +0530

    Location marker now accepts fetched markers and captures onclick events

--- app/src/main/java/com/agzuniverse/agz/opensalve/HomeActivity.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/HomeActivity.java
@@ -1,17 +1,27 @@
 package com.agzuniverse.agz.opensalve;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
+import com.agzuniverse.agz.opensalve.Modals.LocationMarker;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
 
     private MapView mapView;
+    private List<LocationMarker> locations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +43,41 @@ public class HomeActivity extends AppCompatActivity {
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
-
+                configMapOverlay(mapboxMap);
             }
         });
 
 //        Intent debug = new Intent(this, CollectionCentreScreen.class);
 //        this.startActivity(debug);
+    }
+
+    public void configMapOverlay(MapboxMap map) {
+
+        //TODO fetch coordinates of all the markers from API
+        String[] titles = new String[]{"MEC", "RSET", "RSC", "GSouk"};
+        String[] snippets = new String[]{"1", "2", "3", "4"};
+        Double[] lats = new Double[]{9.9323, 9.9306, 9.9310, 9.9339};
+        Double[] lngs = new Double[]{76.2633, 76.2653, 76.2673, 76.2693};
+        for (int i = 0; i < titles.length && i < snippets.length && i < lats.length && i < lngs.length; i++) {
+            LocationMarker current = new LocationMarker(titles[i], snippets[i], lats[i], lngs[i]);
+            locations.add(current);
+        }
+
+        for (LocationMarker a : locations) {
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(a.getLat(), a.getLng()))
+                    .title(a.getTitle())
+                    .snippet(a.getSnippet())
+            );
+        }
+
+        map.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+                Toast.makeText(HomeActivity.this, marker.getSnippet(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 
     public void goToGetHelpScreen(View v) {
