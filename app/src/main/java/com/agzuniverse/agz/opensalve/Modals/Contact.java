f226353ae745f2f16ac0afeccffbe4e29a990623
Aswin G <aswinganesh666@gmail.com>
2018-10-13 21:24:48 +0530

    Added contacts model for helpline page

---
+++ app/src/main/java/com/agzuniverse/agz/opensalve/Modals/Contact.java
@@ -0,0 +1,11 @@
+package com.agzuniverse.agz.opensalve.Modals;
+
+public class Contact {
+    private String desc;
+    private String number;
+
+    public Contact(String desc, String number) {
+        this.desc = desc;
+        this.number = number;
+    }
+}
