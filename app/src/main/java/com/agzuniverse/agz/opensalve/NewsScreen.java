fbb1d0ecb08142d591776cdb037aa1af4fba7762
Aswin G <aswinganesh666@gmail.com>
2018-11-19 21:22:49 +0530

    Add FAB to news screen

--- app/src/main/java/com/agzuniverse/agz/opensalve/NewsScreen.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/NewsScreen.java
@@ -1,10 +1,14 @@
 package com.agzuniverse.agz.opensalve;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.agzuniverse.agz.opensalve.Modals.News;
 import com.agzuniverse.agz.opensalve.ViewModels.NewsViewModel;
@@ -13,6 +17,8 @@ import com.agzuniverse.agz.opensalve.adapters.NewsAdapter;
 import java.util.List;
 
 public class NewsScreen extends AppCompatActivity {
+    private String token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +32,20 @@ public class NewsScreen extends AppCompatActivity {
         RecyclerView.Adapter newsAdapter = new NewsAdapter(this, data, getColors());
         news.setAdapter(newsAdapter);
         news.setLayoutManager(newsLayoutManager);
+
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        if (prefs.getInt("isVolunteer", 0) == 1) {
+            LinearLayout f = findViewById(R.id.news_fab_wrapper);
+            f.setVisibility(View.VISIBLE);
+            token = prefs.getString("token", "0");
+        }
     }
 
     private int[] getColors() {
         return getResources().getIntArray(R.array.card_backgrounds);
+    }
+
+    public void addNewNews() {
+
     }
 }
