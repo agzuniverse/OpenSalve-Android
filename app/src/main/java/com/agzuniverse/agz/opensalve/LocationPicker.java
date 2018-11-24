085f265827f4182bfd1771953b70149f85a3030a
Aswin G <aswinganesh666@gmail.com>
2018-11-24 13:44:12 +0530

    Remove geocoding attempt, finish location selection

--- app/src/main/java/com/agzuniverse/agz/opensalve/LocationPicker.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/LocationPicker.java
@@ -1,11 +1,10 @@
 package com.agzuniverse.agz.opensalve;
 
 import android.graphics.PointF;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +15,13 @@ import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
 
 public class LocationPicker extends AppCompatActivity {
+    private MapboxMap map;
+    private Marker addressPin;
+    private ImageView dropPinView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +30,9 @@ public class LocationPicker extends AppCompatActivity {
         MapView mapView = findViewById(R.id.locationPickerMap);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(mapboxMap -> {
+            map = mapboxMap;
             // Create drop pin using custom image and statically Set drop pin in center of screen
-            ImageView dropPinView = new ImageView(this);
+            dropPinView = new ImageView(this);
             dropPinView.setImageResource(R.drawable.ic_location_on);
             FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
@@ -39,67 +40,31 @@ public class LocationPicker extends AppCompatActivity {
             params.bottomMargin = (int) (12 * density);
             dropPinView.setLayoutParams(params);
             mapView.addView(dropPinView);
-            //TODO on confirm location do the following
-            dropPinView.setVisibility(View.INVISIBLE);
-            // Get LatLng of selected location
-            LatLng position = mapboxMap.getProjection().fromScreenLocation(
-                    new PointF(dropPinView.getLeft() + (dropPinView.getWidth() / 2), dropPinView.getBottom())
-            );
 
-            // Remove previous address pin (if exists)
-//            if (addressPin != null) {
-//                if (mapboxMap != null && addressPin != null) {
-//                    mapboxMap.removeMarker(addressPin);
+//            Geocoder gc = new Geocoder(this);
+//            if (gc.isPresent()) {
+//                List<Address> list = null;
+//                try {
+//                    list = gc.getFromLocation(76.2673, 9.9312, 1);
+//                    Log.i("zxcv", list.toString());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
 //                }
+//                Address address = list.get(0);
+//                StringBuffer str = new StringBuffer();
+//                str.append("Name: " + address.getLocality());
+//                String strAddress = str.toString();
+//                addressPin.setTitle(strAddress);
 //            }
-
-//          Create new address pin
-            Marker addressPin = mapboxMap.addMarker(new MarkerOptions().title("Loading address…").position(position));
-            mapboxMap.selectMarker(addressPin);
-            Geocoder gc = new Geocoder(this, Locale.getDefault());
-            if (gc.isPresent()) {
-                List<Address> list = null;
-                try {
-                    list = gc.getFromLocation(position.getLongitude(), position.getLatitude(), 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Address address = list.get(0);
-                StringBuffer str = new StringBuffer();
-                str.append("Name: " + address.getLocality());
-                String strAddress = str.toString();
-                addressPin.setTitle(strAddress);
-            }
-            // Start the Geocoding…
-            //Create Geocoding client
-//            MapboxGeocoder client = new MapboxGeocoder.Builder()
-//                    .setAccessToken(getResources().getString(R.string.mapbox_api_token))
-//                    .setCoordinates(position.getLongitude(), position.getLatitude())
-//                    .setType(GeocoderCriteria.TYPE_ADDRESS)
-//                    .build();
-//
-//            //Place the request
-//            client.enqueue(new Callback<GeocoderResponse>() {
-//                @Override
-//                public void onResponse(Call call, Response response) {
-//                    List<GeocoderFeature> results = response.body().getClass();
-//                    String address;
-//                    if (results.size() > 0) {
-//                        GeocoderFeature feature = results.get(0);
-//                        address = feature.getPlaceName() + " " + feature.getText();
-//                        //            Update Address Pin's InfoWindow With Address
-//                        if (addressPin != null) {
-//                            addressPin.setTitle(TextUtils.isEmpty(address) ? "No address found" : address);
-//                        }
-//                    }
-//                }
-//
-//                @Override
-//                public void onFailure(Call<GeocoderResponse> call, Throwable t) {
-//                }
-//            });
         });
+    }
 
-
+    public void confirmLocation(View v) {
+        dropPinView.setVisibility(View.INVISIBLE);
+        LatLng position = map.getProjection().fromScreenLocation(
+                new PointF(dropPinView.getLeft() + (dropPinView.getWidth() / 2), dropPinView.getBottom())
+        );
+        addressPin = map.addMarker(new MarkerOptions().position(position));
+        map.selectMarker(addressPin);
     }
 }
