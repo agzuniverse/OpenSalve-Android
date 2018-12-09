a703acad5ac82f8210330829186dbabb12620b90
Aswin G <aswinganesh666@gmail.com>
2018-12-09 18:21:44 +0530

    Integrate news endpoint

--- app/src/main/java/com/agzuniverse/agz/opensalve/NewsScreen.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/NewsScreen.java
@@ -3,6 +3,8 @@ package com.agzuniverse.agz.opensalve;
 import android.app.Dialog;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -18,21 +20,32 @@ import com.agzuniverse.agz.opensalve.ViewModels.NewsViewModel;
 import com.agzuniverse.agz.opensalve.adapters.NewsAdapter;
 import com.agzuniverse.agz.opensalve.widgets.AddNewsDialog;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class NewsScreen extends AppCompatActivity implements AddNewsDialog.AddNewsSubmit {
     private String token;
     private RecyclerView.Adapter newsAdapter;
     private List<News> data;
     private boolean showCloseButton = false;
+    private NewsViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news);
 
-        NewsViewModel model = ViewModelProviders.of(this).get(NewsViewModel.class);
-        data = model.getNews();
+        model = ViewModelProviders.of(this).get(NewsViewModel.class);
+        getNewsAsync();
 
         FloatingActionButton fab = findViewById(R.id.news_fab);
         fab.setOnClickListener((View view) -> addNewNews());
@@ -43,11 +56,26 @@ public class NewsScreen extends AppCompatActivity implements AddNewsDialog.AddNe
             showCloseButton = true;
         }
 
-        RecyclerView news = findViewById(R.id.news_list);
-        RecyclerView.LayoutManager newsLayoutManager = new LinearLayoutManager(this);
-        newsAdapter = new NewsAdapter(this, data, getColors(), showCloseButton);
-        news.setAdapter(newsAdapter);
-        news.setLayoutManager(newsLayoutManager);
+
+    }
+
+    public void getNewsAsync() {
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                RecyclerView news = findViewById(R.id.news_list);
+                RecyclerView.LayoutManager newsLayoutManager = new LinearLayoutManager(NewsScreen.this);
+                newsAdapter = new NewsAdapter(NewsScreen.this, data, getColors(), showCloseButton);
+                news.setAdapter(newsAdapter);
+                news.setLayoutManager(newsLayoutManager);
+            }
+        };
+        Runnable runnable = () -> {
+            data = model.getNews(getResources().getString(R.string.base_api_url));
+            handler.sendEmptyMessage(0);
+        };
+        Thread async = new Thread(runnable);
+        async.start();
     }
 
     private int[] getColors() {
@@ -69,8 +97,29 @@ public class NewsScreen extends AppCompatActivity implements AddNewsDialog.AddNe
         News news = new News(body, author, 0);
         data.add(news);
         newsAdapter.notifyItemInserted(data.size());
+        JSONObject json = new JSONObject();
+        try {
+            json.put("title", news.getAuthor());
+            json.put("contents", news.getBody());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         Runnable runnable = () -> {
-            //TODO send new news to backend
+            OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).build();
+            RequestBody reqbody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json.toString());
+            Request request = new Request.Builder()
+                    .url(getResources().getString(R.string.base_api_url) + "/api/news/")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Token " + GlobalStore.token)
+                    .post(reqbody)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                JSONObject res = new JSONObject(response.body().string());
+                
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
         };
         Thread async = new Thread(runnable);
         async.start();
--- app/src/main/java/com/agzuniverse/agz/opensalve/ViewModels/NewsViewModel.java
+++ app/src/main/java/com/agzuniverse/agz/opensalve/ViewModels/NewsViewModel.java
@@ -4,18 +4,47 @@ import android.arch.lifecycle.ViewModel;
 
 import com.agzuniverse.agz.opensalve.Modals.News;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NewsViewModel extends ViewModel {
     private List<News> news = new ArrayList<>();
 
-    public List<News> getNews() {
-        //TODO fetch contacts from backend API
-        News contact1 = new News("Electricity to Maradu will be cut off at midnight", "KSEB Engineer", 1);
-        News contact2 = new News("All educational institutions including professional colleges will have a holiday tomorrow", "Ernakulam District Collector", 2);
-        news.add(contact1);
-        news.add(contact2);
+    public List<News> getNews(String apiUrl) {
+        OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).build();
+        Request request = new Request.Builder().url(apiUrl + "/api/news/").build();
+        try {
+            Response response = client.newCall(request).execute();
+            parse(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        News contact1 = new News("Electricity to Maradu will be cut off at midnight", "KSEB Engineer", 1);
+//        News contact2 = new News("All educational institutions including professional colleges will have a holiday tomorrow", "Ernakulam District Collector", 2);
+//        news.add(contact1);
+//        news.add(contact2);
         return news;
+    }
+
+    public void parse(String data) {
+        try {
+            JSONArray json = new JSONObject(data).getJSONArray("results");
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject j = json.getJSONObject(i);
+                News curr = new News(j.getString("contents"), j.getString("title"), j.getInt("id"));
+                news.add(curr);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
